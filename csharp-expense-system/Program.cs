70dfaa14a3e697feca12af412f8c97f27de38217
João Sampaio <148175806+JoaoMarcu@users.noreply.github.com>
2026-03-26 21:03:59 -0300

    Inicio

---
+++ csharp-expense-system/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Gasto> gastos = new List<Gasto>();
+        int proximoId = 1;
+
+        int opcao = -1;
+
+        while (opcao != 0)
+        {
+            Console.WriteLine("1 - Adicionar gasto");
+            Console.WriteLine("2 - Listar gastos");
+            Console.WriteLine("3 - Atualizar gasto");
+            Console.WriteLine("4 - Remover gasto");
+            Console.WriteLine("5 - Total gasto");
+            Console.WriteLine("0 - Sair");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+
+            {
+                case 1:
+                    Gasto novo = new Gasto();
+
+                    novo.Id = proximoId++;
+
+                    Console.Write("Nome: ");
+                    novo.Nome = Console.ReadLine();
+
+                    Console.Write("Valor: ");
+                    novo.Valor = double.Parse(Console.ReadLine());
+
+                    Console.Write("Categoria: ");
+                    novo.Categoria = Console.ReadLine();
+
+                    gastos.Add(novo);
+
+                    Console.WriteLine("Gasto adicionado!");
+                    break;
+                case 2:
+                    foreach (Gasto g in gastos)
+                    {
+                        Console.Write($" {g.Id} | {g.Nome} | {g.Valor} | {g.Categoria} | {g.Data} |\n");
+                        Console.WriteLine();
+
+                    }
+                        
+                    break;
+            }
+        }
+
+        
+    }
+    
+}
+
+public class Gasto
+{
+    public int Id;
+    public string Nome = null!;
+    public double Valor;
+    public string Categoria = null!;
+    public DateTime Data;
+
+    
+}
